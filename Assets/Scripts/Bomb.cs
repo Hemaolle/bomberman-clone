cf96823390289e7bb3a9eede32a5fd29c687f53c
Oskari Leppäaho <oskari.leppaaho@gmail.com>
2016-03-11 19:13:52 +0200

    Bomb first version
    -Player can drop bombs
    -Limited number of bombs alive at the same time
    -The bombs destroy themselves after certain time

---
+++ Assets/Scripts/Bomb.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class Bomb : MonoBehaviour
+{
+	public float fuseTime;
+
+	public event Action BombExplode;
+
+	void Start() {
+		Invoke("Explode", fuseTime);
+	}
+
+	void Explode() {		
+		BombExplode();
+		Destroy(gameObject);
+	}
+}
+
---
+++ Assets/Scripts/PlayerDropBomb.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerDropBomb : MonoBehaviour {
+	public GameObject bomb;
+	public int maxLiveBombs = 1;
+
+	private int numberOfBombs = 0;
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown("Fire1") && numberOfBombs < maxLiveBombs) {
+			var newBomb = (GameObject)Instantiate(bomb, transform.position, Quaternion.identity);
+			newBomb.GetComponent<Bomb>().BombExplode += OnBombExplode;
+			numberOfBombs++;
+		}
+	}
+
+	public void OnBombExplode() {
+		numberOfBombs--;
+	}
+}

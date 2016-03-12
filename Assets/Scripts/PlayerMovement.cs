c59e26314a883313d116145160867ee7ccadaf06
Oskari Leppäaho <oskari.leppaaho@gmail.com>
2016-03-12 08:28:59 +0200

    Player movement in just 4 directions
    Has a bug: if player first holds for example up, then holds right at the same time
    and then released right, but still holds up, the movement will stop.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -10,10 +10,50 @@ public class PlayerMovement : MonoBehaviour {
 	void Start () {
 		myRigidbody2D = GetComponent<Rigidbody2D>();
 	}
-	
+
+	enum MovementDirections {
+		None, Up, Down, Left, Right
+	};
+
+	private MovementDirections latestButtonPressed = MovementDirections.None;
+
 	// Update is called once per frame
 	void Update () {
-		Vector2 newVelocity = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
-		myRigidbody2D.velocity = newVelocity * speed;
+		MovementDirections buttonPressedInThisFrame = GetInputDirectionPressed();
+		if (buttonPressedInThisFrame != MovementDirections.None) {
+			latestButtonPressed = buttonPressedInThisFrame;
+		}
+
+		MovementDirections buttonReleasedInThisFrame = GetInputDirectionReleased();
+		if (latestButtonPressed == buttonReleasedInThisFrame) {
+			latestButtonPressed = MovementDirections.None;
+		}
+
+		Move();
+	}
+
+	MovementDirections GetInputDirectionPressed() {
+		if (Input.GetButtonDown("Left")) return MovementDirections.Left;
+		if (Input.GetButtonDown("Right")) return MovementDirections.Right;
+		if (Input.GetButtonDown("Up")) return MovementDirections.Up;
+		if (Input.GetButtonDown("Down")) return MovementDirections.Down;
+		return MovementDirections.None;
+	}
+
+	MovementDirections GetInputDirectionReleased() {
+		if (Input.GetButtonUp("Left")) return MovementDirections.Left;
+		if (Input.GetButtonUp("Right")) return MovementDirections.Right;
+		if (Input.GetButtonUp("Up")) return MovementDirections.Up;
+		if (Input.GetButtonUp("Down")) return MovementDirections.Down;
+		return MovementDirections.None;
+	}
+
+	private void Move() {
+		Vector2 movementDirection = new Vector2(0, 0);
+		if (latestButtonPressed == MovementDirections.Left)  movementDirection = new Vector2(-1,  0);
+		if (latestButtonPressed == MovementDirections.Right) movementDirection = new Vector2( 1,  0);
+		if (latestButtonPressed == MovementDirections.Up)    movementDirection = new Vector2( 0,  1);
+		if (latestButtonPressed == MovementDirections.Down)  movementDirection = new Vector2( 0, -1);
+		myRigidbody2D.velocity = movementDirection * speed;
 	}
 }

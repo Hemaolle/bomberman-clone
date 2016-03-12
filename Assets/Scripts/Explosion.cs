30c69aa7d990645c6159f75f170fcfb3475bf6ac
Oskari Leppäaho <oskari.leppaaho@gmail.com>
2016-03-12 07:38:25 +0200

    The explosions destroy ground

---
+++ Assets/Scripts/Explosion.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Explosion : MonoBehaviour {
+	void OnTriggerEnter2D(Collider2D collider) {		
+		if (collider.gameObject.tag == "Ground")
+			Destroy(collider.gameObject);
+	}
+}

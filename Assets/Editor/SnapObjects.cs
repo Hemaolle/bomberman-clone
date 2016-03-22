030f01c0d8cc8811e3726df8cf710d7653785e69
Oskari Leppäaho <oskari.leppaaho@gmail.com>
2016-03-22 07:42:19 +0200

    Add an editor script for snapping game objects

---
+++ Assets/Editor/SnapObjects.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+public class SnapObjects {
+
+	[MenuItem("Oskari/Snap objects' x and y coordinates to 0.5")]
+	private static void Snap() {
+		GameObject[] selectedObjects = Selection.gameObjects;
+		foreach (GameObject selectedObject in selectedObjects) {
+			Vector3 localPosition = selectedObject.transform.localPosition;
+			localPosition.x = Mathf.Floor(localPosition.x) + 0.5f;
+			localPosition.y = Mathf.Floor(localPosition.y) + 0.5f;
+			selectedObject.transform.localPosition = localPosition;
+		}
+	}
+}
